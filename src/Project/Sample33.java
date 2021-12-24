8b062e0cf2a3d6516284334fb20ebf55e249c038
Anita <yadavanita632@gmail.com>
2021-12-25 00:05:58 +0530

    this is second project

---
+++ src/Project/Sample33.java
@@ -0,0 +1,13 @@
+package Project;
+
+public class Sample33
+{
+  public static void main(String[] args) 
+  {
+	System.out.println("gm");
+	System.out.println("gm");
+	System.out.println("gm");
+	System.out.println("gm");
+	System.out.println("gm");
+}
+}
